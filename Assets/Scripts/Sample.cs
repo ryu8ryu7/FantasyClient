177253002601040cabdc230f95bc54f718a8ed48
DESKTOP-F632G4Q\ooi11 <ryu8ryu7@yahoo.co.jp>
2022-10-03 00:47:49 +0900

    修正

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour
 {
+    public CharacterBase _character = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class Sample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetMouseButtonDown( 0 ) )
+        {
+            _character.PlayAnimation(AnimationSetScriptableObject.AnimationSetNameLabel.Fighting_Combo01, AnimationSetScriptableObject.AnimationSetNameLabel.Idle01);
+        }
     }
 }
